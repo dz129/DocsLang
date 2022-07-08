962d524eb0b1e4d32da4d9294e64683f10ba209c
dz129 <106422247+dz129@users.noreply.github.com>
2022-07-08 10:34:47 -0400

    Update parser.cpp
    print statements added

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,7 @@
 #include "lexer.h"
 #include "parser.h"
 #include <vector>
+#include <stack>
 #include <iostream>
 
 class parser{
@@ -40,19 +41,67 @@ class parser{
     }
 
     void fillPrint(int vecIndex, std::vector<Token> tokenvec, printTree* root){
-        if (vecIndex >= tokenvec.size()) return;
-        printTree* newprint = new printTree;
-        tokenvec[vecIndex].contents = 
+        std::stack<std::string> stringstack;
+        std::stack<std::string> opstack;
+        if (tokenvec.size() <= 2){
+            printRoot -> print = tokenvec[1].contents;
+            return;
+            }
+        else{
+            printRoot -> print = tokenvec[1].contents;
+            printRoot -> next = new printTree;
+            printTree* temp = printRoot -> next;
+            for (int i = 2; i < tokenvec.size(); i++) {
+                if (tokenvec[i].type == STRING){
+                    temp -> print = tokenvec[i].contents;
+                    temp -> next = new printTree;
+                    temp = temp -> next;
+                }
+                else if (tokenvec[i].type == PLUS){
+                    temp -> operation = tokenvec[i].type;
+                    temp -> next = new printTree;
+                    temp = temp -> next;
+                }
+                else{
+                    printRoot -> syntaxError = true;
+                }
+            }
+            return;
+        }
     }
+
 
     std::string evalEval(){
 
     }
 
     std::string evalPrint(){
+        std::string returnValue;
+        std::stack<std::string> stringFinal;
+        std::stack<TokenType> operations;
+        printTree* temp = printRoot;
+        if (printRoot -> syntaxError) return "SYNTAX ERROR";
+        while (temp != NULL){
+            if(temp -> operation == PLUS){
+                operations.push(temp -> operation);
+                temp = temp -> next;
+            }
+            else{
+                stringFinal.push(temp -> print);
+                temp = temp -> next;
+                }
 
+        }
+        if (operations.size() != 0 && stringFinal.size() - 1 != operations.size()) return "SYNTAX ERROR";
+        else{
+            while (!stringFinal.empty()){
+                returnValue = returnValue + stringFinal.top();
+                stringFinal.pop();
+            }
+        }
+        return returnValue;
     }
     void print(){
-
+        std::cout << evalPrint();
     }
 };
